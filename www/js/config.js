0376fa6fea33ca124f994831caa2325d52145a84
Gash21 <galiharghubi@gmail.com>
2017-02-01 16:03:09 +0700

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -5,7 +5,7 @@ angular.module('starter.config', [])
 })
 
 .constant('DB_CONFIG', {
-    name: 'seekmi_customer',
+    name: 'table_name',
     version: '1.7',
     tables: [
         {
@@ -25,110 +25,6 @@ angular.module('starter.config', [])
           ]
         },
         {
-          name: 'tblservicetags',
-          columns: [
-              {name: 'id', type: 'integer primary key'},
-              {name: 'tagId', type: 'integer'},
-              {name: 'subserviceId', type: 'integer'},
-              {name: 'serviceTag', type: 'text'},
-              {name: 'isEnglish', type: 'text'},
-              {name: 'createdDate', type: 'text'}
-          ]
-        },
-        {
-          name: 'tblprojects',
-          columns: [
-              {name: 'id', type: 'integer'},
-              {name: 'projectId', type: 'integer primary key'},
-              {name: 'userId', type: 'integer'},
-              {name: 'serviceId', type: 'integer'},
-              {name: 'serviceName', type: 'text'},
-              {name: 'serviceNameInd', type: 'text'},
-              {name: 'status', type: 'text'},
-              {name: 'latitude', type: 'text'},
-              {name: 'longitude', type: 'text'},
-              {name: 'requestLocation', type: 'text'},
-              {name: 'cityName', type: 'text'},
-              {name: 'budgetMin', type: 'text'},
-              {name: 'budgetMax', type: 'text'},
-              {name: 'budgetType', type: 'text'},
-              {name: 'jobId', type: 'text'},
-              {name: 'isReviewed', type: 'text'},
-              {name: 'quoteRequestCount', type: 'integer'},
-              {name: 'lastAccess', type: 'text'},
-              {name: 'voucherCode', type: 'text'},
-              {name: 'campaignId', type: 'integer'},
-              {name: 'modifiedDate', type: 'text'},
-              {name: 'createdDate', type: 'text'}
-          ]
-        },
-        {
-          name: 'tblprojectdetails',
-          columns: [
-              {name: 'id', type: 'integer'},
-              {name: 'projectDetailId', type: 'integer primary key'},
-              {name: 'projectId', type: 'integer'},
-              {name: 'userId', type: 'integer'},
-              {name: 'questionId', type: 'integer'},
-              {name: 'questionName', type: 'text'},
-              {name: 'questionNameInd', type: 'text'},
-              {name: 'optionId', type: 'integer'},
-              {name: 'optionVal', type: 'text'},
-              {name: 'optionValInd', type: 'text'},
-              {name: 'fieldType', type: 'text'},
-              {name: 'extras', type: 'text'},
-              {name: 'orders', type: 'integer'}
-          ]
-        },
-        {
-          name: 'tblquoterequest',
-          columns: [
-              {name: 'id', type: 'integer'},
-              {name: 'requestId', type: 'integer primary key'},
-              {name: 'projectId', type: 'integer'},
-              {name: 'userId', type: 'integer'},
-              {name: 'prosId', type: 'integer'},
-              {name: 'prosFirstName', type: 'text'},
-              {name: 'prosLastName', type: 'text'},
-              {name: 'prosUserPhoto', type: 'text'},
-              {name: 'status', type: 'text'},
-              {name: 'businessName', type: 'text'},
-              {name: 'quotePrice', type: 'text'},
-              {name: 'priceType', type: 'text'},
-              {name: 'quoteMessage', type: 'text'},
-              {name: 'recentMessage', type: 'text'},
-              {name: 'reviewCount', type: 'integer'},
-              {name: 'rating', type: 'text'},
-              {name: 'isDeleted', type: 'text'},
-              {name: 'isArchived', type: 'text'},
-              {name: 'lastChatMessageDate', type: 'text'},
-              {name: 'readByVendorFlag', type: 'text'},
-              {name: 'readByUserFlag', type: 'text'},
-              {name: 'userFlagUnread', type: 'integer'},
-              {name: 'modifiedDate', type: 'text'},
-              {name: 'createdDate', type: 'text'}
-          ]
-        },
-        {
-          name: 'tblquotemessage',
-          columns: [
-              {name: 'id', type: 'integer'},
-              {name: 'requestId', type: 'integer'},
-              {name: 'projectId', type: 'integer'},
-              {name: 'userId', type: 'integer'},
-              {name: 'messageId', type: 'integer primary key'},
-              {name: 'messageFrom', type: 'integer'},
-              {name: 'messageTo', type: 'integer'},
-              {name: 'message', type: 'text'},
-              {name: 'readFlag', type: 'text'},
-              {name: 'time_left', type: 'text'},
-              {name: 'quotePriceText', type: 'text'},
-              {name: 'readByVendorFlag', type: 'text'},
-              {name: 'readByUserFlag', type: 'text'},
-              {name: 'createdDate', type: 'text'}
-          ]
-        },
-        {
           name: 'tblsynctimes',
           columns: [
               {name: 'tableName', type: 'text primary key'},
@@ -141,20 +37,14 @@ angular.module('starter.config', [])
     $ionicConfigProvider.tabs.style("standard");
 })
 
-//.constant('GCM_SENDER_ID', '245412752281'); // GCM UAT
-.constant('GCM_SENDER_ID', '674518732620') // GCM PRODUCTION
-
-.constant('IMG_URL', 'https://seekmi.com/images/mobile/') // PRODUCTION
-// .constant('IMG_URL', 'http://uat.seekmi.com/images/mobile/'); // UAT
-
 var appConfig = {
     //PRODUCTION
-//   api_base_url: 'https://apic.seekmi.com/vendor/v1',
-//   image_api_url: 'https://apic.seekmi.com/seekmi/'
+//   api_base_url: 'http://url.com/vendor/v1/',
+//    image_api_url: 'https://url.com/endpoint/'
 
     //UAT
-    api_base_url: 'http://apic-uat.seekmi.com/vendor/v1/',
-    image_api_url: 'https://www.seekmi.com/seekmi/'
+    api_base_url: 'http://url.com/vendor/v1/',
+    image_api_url: 'https://url.com/endpoint/'
 
 
 
